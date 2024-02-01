a76273cb8e75f6977a4242bc51948b7c8d66f710
AJ ONeal <aj@therootcompany.com>
2024-01-31 18:06:27 -0700

    doc: export type 'PkgConfig'

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ module.exports = require("./lib/jswt.js")
 
 // these typedef reexports will be available to dependent packages
 /**
+ * @typedef {import('./bin/jswt-init.js').PkgConfig} PkgConfig
  * @typedef {import('./bin/jswt-init.js').MyError} MyError
  * @typedef {import('./bin/jswt-reexport.js').Typedef} Typedef
  */
